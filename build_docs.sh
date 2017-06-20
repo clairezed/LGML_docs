804299bd018a35feb50cfc0e38da304d7762167f
clairezed <clairezuliani@gmail.com>
2017-06-20 15:17:41 +0200

    build script updat

--- build_docs.sh
+++ build_docs.sh
@@ -1,4 +1,7 @@
+echo "starting build ==============================="
 cd docs
 ls -l
 bundle install
+echo "bundle installed ============================="
 bundle exec jekyll build
+echo "ending build ================================="
